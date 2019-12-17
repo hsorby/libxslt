dfb8f56d4a5a6388acbb24370fe9ad1b0e3a62d1
Hugh Sorby <h.sorby@auckland.ac.nz>
2019-12-17 15:10:08 +1300

    Add CMake files to generate build instructions for all platforms.

---
+++ cmake/config.in.h
@@ -0,0 +1,58 @@
+
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+#cmakedefine HAVE_CLOCK_GETTIME
+#cmakedefine HAVE_CRYPTO
+#cmakedefine HAVE_CTYPE_H
+#cmakedefine HAVE_ERRNO_H
+#cmakedefine HAVE_FCNTL_H
+#cmakedefine HAVE_FLOAT_H
+#cmakedefine HAVE_FPRINTF
+#cmakedefine HAVE_FSCANF
+#cmakedefine HAVE_FTIME
+#cmakedefine HAVE_GCRYPT
+#cmakedefine HAVE_GETTIMEOFDAY
+#cmakedefine HAVE_GMTIME
+#cmakedefine HAVE_GMTIME_R
+#cmakedefine HAVE_IEEEFP_H
+#cmakedefine HAVE_ISINF
+#cmakedefine HAVE_ISNAN
+#cmakedefine HAVE_LOCALE_H
+#cmakedefine HAVE_LOCALTIME
+#cmakedefine HAVE_LOCALTIME_R
+#cmakedefine HAVE_MALLOC_H
+#cmakedefine HAVE_MATH_H
+#cmakedefine HAVE_NAN_H
+#cmakedefine HAVE_PRINTF
+#cmakedefine HAVE_PTHREAD_H
+#cmakedefine HAVE_SCANF
+#cmakedefine HAVE_SNPRINTF
+#cmakedefine HAVE_SPRINTF
+#cmakedefine HAVE_SSCANF
+#cmakedefine HAVE_STAT
+#cmakedefine HAVE_STDARG_H
+#cmakedefine HAVE_STDINT_H
+#cmakedefine HAVE_STDLIB_H
+#cmakedefine HAVE_STRING_H
+#cmakedefine HAVE_STRXFRM_L
+#cmakedefine HAVE_SYS_SELECT_H
+#cmakedefine HAVE_SYS_STAT_H
+#cmakedefine HAVE_SYS_TIMEB_H
+#cmakedefine HAVE_SYS_TIME_H
+#cmakedefine HAVE_SYS_TYPES_H
+#cmakedefine HAVE_TIME
+#cmakedefine HAVE_TIME_H
+#cmakedefine HAVE_UNISTD_H
+#cmakedefine HAVE_VFPRINTF
+#cmakedefine HAVE_VFSCANF
+#cmakedefine HAVE_VPRINTF
+#cmakedefine HAVE_VSCANF
+#cmakedefine HAVE_VSNPRINTF
+#cmakedefine HAVE_VSPRINTF
+#cmakedefine HAVE_VSSCANF
+#cmakedefine HAVE_XLOCALE_H
+#cmakedefine HAVE__STAT
+
+#endif /* _CONFIG_H_ */
+
